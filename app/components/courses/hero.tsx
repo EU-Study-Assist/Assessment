83acfc07d5437a1808065923c41c717fe72dc769
justlogicalahmad <rabiu2993@gmail.com>
2024-09-16 13:01:43 -0700

    Add: Responsive hero section

--- app/components/courses/hero.tsx
+++ app/components/courses/hero.tsx
@@ -1,11 +1,19 @@
+import RegInput from "@/app/ui/reg-input";
 import React from "react";
 
 const Hero = () => {
   return (
-    <div className="flex justify-center text-center">
-      <p>Free Courses</p>
-      <h2>Introduction to R, SQL and Python</h2>
-      <p>Level up your data skills with EU study assist</p>
+    <div className="flex justify-center text-center m-4 lg:my-10 lg:px-14 w-full lg:h-[80vh]">
+      <div className="rounded border bg-primary text-white flex-1 h-80 lg:h-full flex flex-col text-center justify-center items-center">
+        <p className="text-sm mb-1 lg:mb-3">Free Courses</p>
+        <h2 className="text-3xl lg:text-4xl px-4 lg:w-[30vw] font-semibold">
+          Introduction to R, SQL and Python
+        </h2>
+        <p className="text-xs mt-3">
+          Level up your data skills with EU study assist
+        </p>
+        <RegInput />
+      </div>
     </div>
   );
 };
---
+++ app/ui/reg-input.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const RegInput = () => {
+  return (
+    <div className="bg-white flex items-center justify-between rounded-full px-2 ps-4 py-2 lg:py-1 mt-5 lg:mt-8 lg:w-[27vw]">
+      <input
+        className="border-none outline-none text-xs lg:text-sm flex-1 lg:px-2 text-black"
+        placeholder="Enter your Email"
+        type="email"
+        alt="Email Input"
+      />
+      <button className="bg-primary px-3 rounded-full py-2 text-xs lg:text-sm lg:px-4">
+        Register for Free
+      </button>
+    </div>
+  );
+};
+
+export default RegInput;
